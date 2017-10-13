af093de6496cea5a13054bc12b6f59bcfcbdb626
Matej Štorga <matej.storga@fer.hr>
2017-10-13 16:50:12 +0200

    Dodan projekt

---
+++ CustomStruktura/IntegerList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStruktura
+{
+
+    public class IntegerList : IIntegerList
+    {
+        private int[] _internalStorage;
+
+        public int Count => throw new NotImplementedException();
+
+        public void Add(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetElement(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+        // ... IIntegerList implementation ...
+    }
+}
