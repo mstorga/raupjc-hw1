c908cf12cf9a43616af7e315b407f9f8926fc60e
Matej Štorga <matej.storga@fer.hr>
2017-10-14 17:08:59 +0200

    1 zadatak gotov

--- CustomStruktura/Program.cs
+++ CustomStruktura/Program.cs
@@ -10,6 +10,20 @@ namespace CustomStruktura
     {
         static void Main(string[] args)
         {
+            IIntegerList listOfIntegers = new IntegerList();
+            listOfIntegers.Add(1); // [1]
+            listOfIntegers.Add(2); // [1 ,2]
+            listOfIntegers.Add(3); // [1 ,2 ,3]
+            listOfIntegers.Add(4); // [1 ,2 ,3 ,4]
+            listOfIntegers.Add(5); // [1 ,2 ,3 ,4 ,5]
+            listOfIntegers.RemoveAt(0); // [2 ,3 ,4 ,5]
+            listOfIntegers.Remove(5); // [2 ,3 ,4]
+            Console.WriteLine(listOfIntegers.Count); // 3
+            Console.WriteLine(listOfIntegers.Remove(100)); // false
+            Console.WriteLine(listOfIntegers.RemoveAt(5)); // false
+            listOfIntegers.Clear(); // []
+            Console.WriteLine(listOfIntegers.Count); // 0
         }
+
     }
 }
